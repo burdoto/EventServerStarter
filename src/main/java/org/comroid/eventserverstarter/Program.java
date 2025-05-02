6949a452777341bfa7f2d730d091ab2c12efafb8
Kaley <kaleidox@comroid.org>
2025-05-02 15:27:16 +0200

    shutdown command

--- src/main/java/org/comroid/eventserverstarter/Program.java
+++ src/main/java/org/comroid/eventserverstarter/Program.java
@@ -100,6 +100,13 @@ public class Program extends Component.Base {
         }};
     }
 
+    @Command(permission = "8")
+    public String shutdown() {
+        terminate();
+        System.exit(0);
+        return "Goodbye";
+    }
+
     @Command(permission = "8589934592") // perm: MANAGE_EVENTS
     @Description("Link a discord event with a systemd service")
     public String link(
